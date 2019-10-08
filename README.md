8e6dec48a8f49aac817b34adc678e05a0cea664e
Heejin Ohn <heejin-ohn@ou.edu>
2019-10-07 21:34:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Self-Study of *Data Visualization (2019)* by Kieran Healy
+    This repository contains practice files that I generated as followed the
+    book by Kieran Healy. It mostly contains exercises in *R* along with any
+   auxilary files produced during my study.
+   
+
+## 
---
+++ r_Files/00_preface.r
@@ -0,0 +1,10 @@
+# Package required for the book
+myPackages <- c("tidyverse", "broom", "coefplot", "cowplot", "gapminder", "GGally", "ggrepel", "ggridges", "gridExtra", "here", "interplot", "margins", "maps", "mapproj", "mapdata", "MASS", "quantreg", "rlang", "scales", "survey", "srvyr", "viridis", "viridisLite", "devtools")
+install.packages(myPackages, repos="http://cran.rstudio.com")
+
+
+# Book materials 
+devtools::install_github("kjhealy/socviz")
+
+
+
